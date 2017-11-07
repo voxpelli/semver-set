21112730f7bb842d82a5ce16445c83ffb9533803
Pelle Wessman <pelle@kodfabrik.se>
2017-11-07 10:46:36 +0100

    More tests + fix two incorrect resolutions

--- test/main.spec.js
+++ test/main.spec.js
@@ -17,6 +17,21 @@ describe('intersect', () => {
     intersect('^2.2.4', '^2.3.5').should.equal('^2.3.5');
 
     intersect('>=6.0.0', '>=7.0.0').should.equal('>=7.0.0');
+    intersect('>=7.0.0', '>=6.0.0').should.equal('>=7.0.0');
     should.not.exist(intersect('>=6.0.0', '<6.0.0'));
+
+    intersect('>=6.0.0 <7.0.0', '>=6.0.0').should.equal('^6.0.0');
+    intersect('6.x', '>=6.0.0').should.equal('^6.0.0');
+    intersect('^6.1.0', '>=4.0.0').should.equal('^6.1.0');
+    intersect('6.0.0', '>=6.0.0').should.equal('6.0.0');
+
+    intersect('^6.1.0', '*').should.equal('^6.1.0');
+    intersect('*', '>=4.0.0').should.equal('>=4.0.0');
+
+    intersect('>=1.0.0 <7.0.0', '>2.0.0 <=8.2.0').should.equal('>2.0.0 <7.0.0');
+    intersect('1.0.0 - 1.9.1', '1.2.0 - 2.5.0').should.equal('>=1.2.0 <=1.9.1');
+
+    should.not.exist(intersect('^0.2.4', '^0.3.5'));
+    intersect('>=0.2.0 <0.5.3', '^0.4.8').should.equal('^0.4.8');
   });
 });
