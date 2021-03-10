e622c348e032991402c69d6e1fa8c1fbe8b4a004
Pelle Wessman <pelle@kodfabrik.se>
2021-03-10 16:04:09 +0100

    Convey status of invidiual intersect tests

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable mocha/no-setup-in-describe */
 /// <reference types="node" />
 /// <reference types="mocha" />
 /// <reference types="chai" />
@@ -20,36 +21,58 @@ const throwIfNull = (value) => {
   return value;
 };
 
+/**
+ * @param {string[]} ranges
+ * @param {string} target
+ */
+const doTest = (ranges, target) => {
+  it(`"${ranges.join('" and "')}"`, () => {
+    throwIfNull(intersect(...ranges)).should.equal(target);
+  });
+};
+
+/**
+ * @param {string[]} ranges
+ */
+const doNegativeTest = (ranges) => {
+  it(`"${ranges.join('" and "')}"`, () => {
+    should.not.exist(intersect(...ranges));
+  });
+};
+
 describe('intersect', () => {
-  it('basic usage', () => {
-    throwIfNull(intersect('^1.1 || ^2.2 || >=5', '^2.2.0-alpha1')).should.equal('^2.2.0');
+  describe('should intersect', () => {
+    doTest(['^1.1 || ^2.2 || >=5', '^2.2.0-alpha1'], '^2.2.0');
 
-    should.not.exist(intersect('~2.2.4', '~2.3.0'));
-    throwIfNull(intersect('~2.2.4', '~2.2.5')).should.equal('~2.2.5');
-    throwIfNull(intersect('^2.2.4', '~2.2.5')).should.equal('~2.2.5');
+    doTest(['~2.2.4', '~2.2.5'], '~2.2.5');
+    doTest(['^2.2.4', '~2.2.5'], '~2.2.5');
 
-    throwIfNull(intersect('^2.2.4', '^2.3.5')).should.equal('^2.3.5');
+    doTest(['^2.2.4', '^2.3.5'], '^2.3.5');
 
-    throwIfNull(intersect('>=6.0.0', '>=7.0.0')).should.equal('>=7.0.0');
-    throwIfNull(intersect('>=7.0.0', '>=6.0.0')).should.equal('>=7.0.0');
-    should.not.exist(intersect('>=6.0.0', '<6.0.0'));
+    doTest(['>=6.0.0', '>=7.0.0'], '>=7.0.0');
+    doTest(['>=7.0.0', '>=6.0.0'], '>=7.0.0');
 
-    throwIfNull(intersect('>=6.0.0 <7.0.0', '>=6.0.0')).should.equal('^6.0.0');
-    throwIfNull(intersect('6.x', '>=6.0.0')).should.equal('^6.0.0');
-    throwIfNull(intersect('^6.1.0', '>=4.0.0')).should.equal('^6.1.0');
-    throwIfNull(intersect('6.0.0', '>=6.0.0')).should.equal('6.0.0');
+    doTest(['>=6.0.0 <7.0.0', '>=6.0.0'], '^6.0.0');
+    doTest(['6.x', '>=6.0.0'], '^6.0.0');
+    doTest(['^6.1.0', '>=4.0.0'], '^6.1.0');
+    doTest(['6.0.0', '>=6.0.0'], '6.0.0');
 
-    throwIfNull(intersect('^6.1.0', '*')).should.equal('^6.1.0');
-    throwIfNull(intersect('*', '>=4.0.0')).should.equal('>=4.0.0');
+    doTest(['^6.1.0', '*'], '^6.1.0');
+    doTest(['*', '>=4.0.0'], '>=4.0.0');
 
-    throwIfNull(intersect('>=1.0.0 <7.0.0', '>2.0.0 <=8.2.0')).should.equal('>2.0.0 <7.0.0');
-    throwIfNull(intersect('1.0.0 - 1.9.1', '1.2.0 - 2.5.0')).should.equal('>=1.2.0 <=1.9.1');
+    doTest(['>=1.0.0 <7.0.0', '>2.0.0 <=8.2.0'], '>2.0.0 <7.0.0');
+    doTest(['1.0.0 - 1.9.1', '1.2.0 - 2.5.0'], '>=1.2.0 <=1.9.1');
 
-    should.not.exist(intersect('^0.2.4', '^0.3.5'));
-    throwIfNull(intersect('>=0.2.0 <0.5.3', '^0.4.8')).should.equal('^0.4.8');
+    doTest(['>=0.2.0 <0.5.3', '^0.4.8'], '^0.4.8');
 
-    throwIfNull(intersect('>= 4.x <= 10.x', '>=6.0.0')).should.equal('>=6.0.0 <11.0.0-0');
-    throwIfNull(intersect('>= 4.x <= 10.2.4-rc1', '>=6.0.0')).should.equal('>=6.0.0 <=10.2.4-rc1');
-    throwIfNull(intersect('>= 4.x <= 10.2.4-rc1', '>=6.0.0-alpha1 <=10.2.4-rc2')).should.equal('>=6.0.0-alpha1 <=10.2.4-rc1');
+    doTest(['>= 4.x <= 10.x', '>=6.0.0'], '>=6.0.0 <11.0.0-0');
+    doTest(['>= 4.x <= 10.2.4-rc1', '>=6.0.0'], '>=6.0.0 <=10.2.4-rc1');
+    doTest(['>= 4.x <= 10.2.4-rc1', '>=6.0.0-alpha1 <=10.2.4-rc2'], '>=6.0.0-alpha1 <=10.2.4-rc1');
+  });
+
+  describe('should not intersect', () => {
+    doNegativeTest(['~2.2.4', '~2.3.0']);
+    doNegativeTest(['>=6.0.0', '<6.0.0']);
+    doNegativeTest(['^0.2.4', '^0.3.5']);
   });
 });
